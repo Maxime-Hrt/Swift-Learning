8ae44ce3a2c6c36bc5b67e783564be7acaba7339
Maxime <m.77.heurtevent@outlook.com>
2023-03-17 20:07:13 +0100

    want to see the theme

--- elementaryKnowledges.playground/Contents.swift
+++ elementaryKnowledges.playground/Contents.swift
@@ -1,1 +1,11 @@
 import UIKit
+
+let myName = "Maxime" //define constant
+var myAge = 20 //define variable
+myAge = 21
+
+let myJob = "Student" //
+let myYear = 2002
+let isLearning = true
+let myTwitter: String = "@Maxim_hrt"
+
